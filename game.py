23471ec3545f7e0d3fb264199e368800f55602b4
drankilaforge <63487329+drankilaforge@users.noreply.github.com>
2020-06-01 22:18:13 -0700

    Update game.py
    update 1

--- game.py
+++ game.py
@@ -1,27 +1,31 @@
-class HunterRPG: def init(self, character, life, weapon): self.character = character self.life = life self.weapon = weapon
+class HunterRPG:
+    def init(self, character, life, weapon): 
+        self.character = character 
+        self.life = life 
+        self.weapon = weapon
 
-def hunting(self, animal_health):
-    if self.weapon == "Axe":
-        character_damage = 50
-        kill = animal_health - character_damage
-        print(f"Animal health is now {kill} and it was slain by player {self.character} who had the weapon {self.weapon}".format())
+    def hunting(self, animal_health):
+        if self.weapon == "Axe":
+            character_damage = 50
+            kill = animal_health - character_damage
+            print(f"Animal health is now {kill} and it was slain by player {self.character} who had the weapon {self.weapon}".format())
 
-    elif self.weapon == "Katana":
-        character_damage = 75
-        kill = animal_health - character_damage
-        print(f"Animal health is now {kill} and it was slain by player {self.character} who had the weapon {self.weapon}".format())
+        elif self.weapon == "Katana":
+            character_damage = 75
+            kill = animal_health - character_damage
+            print(f"Animal health is now {kill} and it was slain by player {self.character} who had the weapon {self.weapon}".format())
 
-    elif self.weapon == "Crossbow":
-        character_damage = 100
-        kill = animal_health - character_damage
-        print(f"Animal health is now {kill} and it was slain by player {self.character} who had the weapon {self.weapon}".format())
+        elif self.weapon == "Crossbow":
+            character_damage = 100
+            kill = animal_health - character_damage
+            print(f"Animal health is now {kill} and it was slain by player {self.character} who had the weapon {self.weapon}".format())
 
-    else:
-        print(f"Failed to pick a weapon which exists, animal has killed {self.character}")
+        else:
+            print(f"Failed to pick a weapon which exists, animal has killed {self.character}")
 
-def healing(self, potion_power):
-    boosted_health = self.life + potion_power
-    print(f"Your health bars are now {boosted_health}")
+    def healing(self, potion_power):
+        boosted_health = self.life + potion_power
+        print(f"Your health bars are now {boosted_health}")
 
 loop = True 
 while loop: 
